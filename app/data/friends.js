ac8b067dfd32d0aab5ebbba55b3a5d0c6f729c75
Tin Nguyen <Tinman@Tins-MacBook-Pro.local>
2017-07-19 20:14:10 -0500

    adding size for image

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,10 +6,10 @@
 // ===============================================================================
 
 var friendsData = [
-    {"name":"Tin","photo":"aaa","scores":["1","1","1","1","1","1","1","1","1","1"]},
-    {"name":"Tin2","photo":"aaa","scores":["2","1","1","1","1","1","1","1","1","1"]},
-    {"name":"Tin3","photo":"aaa","scores":["3","1","1","1","1","1","1","1","1","1"]},
-    {"name":"Tin4","photo":"aaa","scores":["4","1","1","1","1","1","1","1","1","1"]}
+    {"name":"Tin","photo":"https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg","scores":["1","1","1","1","1","1","1","1","1","1"]},
+    {"name":"Tin2","photo":"https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg","scores":["2","1","1","1","1","1","1","1","1","1"]},
+    {"name":"Tin3","photo":"https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg","scores":["3","1","1","1","1","1","1","1","1","1"]},
+    {"name":"Tin4","photo":"https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg","scores":["4","1","1","1","1","1","1","1","1","1"]}
 ];
 
 // Note how we export the array. This makes it accessible to other files using require.
